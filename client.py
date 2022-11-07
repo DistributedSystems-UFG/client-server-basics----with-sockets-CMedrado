4b021813807183e552fd4ead41794fc006205c82
CMedrado <rafaelcastromedrado@gmail.com>
2022-11-07 17:22:58 -0300

    refactor: fix client and server

--- client.py
+++ client.py
@@ -1,13 +1,18 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *  # -
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('3 4 add'))  # send some data
-s.send(str.encode('3 4 subtract'))  # send some data
-s.send(str.encode('3 2 subtract'))  # send some data
-s.send(str.encode('3 4 multiply'))  # send some data
-s.send(str.encode('3 5 multiply'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))  # connect to server (block until accepted)
+msg = "3 4 add"
+s.send(str.encode(msg))  # send some data
+msg = "3 4 subtract"
+s.send(str.encode(msg))  # send some data
+msg = "3 2 subtract"
+s.send(str.encode(msg))  # send some data
+msg = "3 4 multiply"
+s.send(str.encode(msg))  # send some data
+msg = "3 5 multiply"
+s.send(str.encode(msg))  # send some data
+data = s.recv(1024)  # receive the response
+print(bytes.decode(data))  # print the result
+s.close()  # close the connection
--- server.py
+++ server.py
@@ -4,19 +4,19 @@ from constCS import *  # -
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST, PORT))  # -
 s.listen(1)  # -
+msg = ''
 (conn, addr) = s.accept()  # returns new socket and addr. client 
 while True:  # forever
-  data = conn.recv(1024)  # receive data from client
-  if not data: break  # stop if client stopped
-  tipo = data.split()
-  if tipo[2] == "add":
-    result = float(tipo[1]) + float(tipo[2])
-  if tipo[2] == "subtract":
-    result = float(tipo[1]) - float(tipo[2])
-  if tipo[2] == "multiply":
-    result = float(tipo[1]) * float(tipo[2])
-  else:
-    result = 'invalid input'
-  print("{:.2f}".format(result))
-  conn.send(str.encode("{:.2f}".format(result)))  # return sent data plus an "*"
+    data = conn.recv(1024)  # receive data from client
+    msg = data.decode()
+    if not data: break  # stop if client stopped
+    tipo = msg.split()
+    if tipo[2] == "add":
+        result = float(tipo[0]) + float(tipo[1])
+    elif tipo[2] == "subtract":
+        result = float(tipo[0]) - float(tipo[1])
+    elif tipo[2] == "multiply":
+        result = float(tipo[0]) * float(tipo[1])
+    output = str(result)
+    conn.send(str.encode(output))  # return sent data plus an "*"
 conn.close()  # close the connection
