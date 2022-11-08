b0dc5f288c41c42d78256c27e06099851262e2ac
CMedrado <rafaelcastromedrado@gmail.com>
2022-11-08 11:32:48 -0300

    feat: add input

--- client.py
+++ client.py
@@ -3,16 +3,13 @@ from constCS import *  # -
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT))  # connect to server (block until accepted)
-msg = "3 4 add"
-s.send(str.encode(msg))  # send some data
-msg = "3 4 subtract"
-s.send(str.encode(msg))  # send some data
-msg = "3 2 subtract"
-s.send(str.encode(msg))  # send some data
-msg = "3 4 multiply"
-s.send(str.encode(msg))  # send some data
-msg = "3 5 multiply"
-s.send(str.encode(msg))  # send some data
-data = s.recv(1024)  # receive the response
-print(bytes.decode(data))  # print the result
+print('Digite dois numeros e uma operação(add, multiply, subtract ou divide) separados por espaço ou fim para '
+      'encerrar o programa')
+while True:
+    msg = input()
+    if msg == "fim" or msg == "FIM" or msg == "Fim":
+        break
+    s.send(str.encode(msg))  # send some data
+    data = s.recv(1024)  # receive the response
+    print(bytes.decode(data))  # print the result
 s.close()  # close the connection
